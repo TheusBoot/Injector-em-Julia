da9ca79ae4ad0936ec927dfb463214e9ce17f4a0
Matheus Teixeira Trindade <Emailmartingcf1@gmail.com>
2019-11-02 03:38:06 -0200

    x

--- Injector.jl
+++ Injector.jl
@@ -1,34 +1,44 @@
-
 using Sockets
 
-function connect1(c)
-    println("[-]Cliente Recebido!")
-    request = readline(c)
-    println([#] Cliente request $request)
+function conecta(c, cn)
+	println("\n[#] Client #$cn Received!")
+ 	request = readline(c)
+ 	println("[-] Cliente Request: ", request)
+ 	
+ 	host = strip(split(split(request, ":")[1])[2])
+ 	port = parse(Int32, strip(split(split(request, ":")[2])[1]))
 
-    host = strip(split(split(request,":")[1])[2])
-    port = parse(Int32, strip(split(split(request, ":")[2])[1]))
+ 	s = connect(host, port)
 
-    s = connect(host, port)
+ 	write(c, "HTTP/1.1 200 Established\r\n\r\n") # Send 200 OK to Client.
+ 	
+ 	@async while true
+ 		# Download
+ 		data = readavailable(s)
+ 		length(data) == 0 ? break : Nothing
+ 		write(c, data)
+ 	end
 
-    write(c, "HTTP/1.1 200 estabilisged")
+ 	while true
+ 		# Upload
+ 		data = readavailable(c)
+ 		length(data) == 0 ? break : Nothing
+ 		write(s, data)
+ 	end
 
-    while true
-        # Upload
-        data = readavailable()
-        length(data) == 0 ? break : Nothing
-        write(s, data)
-    and
+ 	println("[!] Client #$cn Disconnected!")
+end
 
-    
+# Listen.
+l = listen(ip"127.0.0.1", 8088)
 
-and
+println("[-] Listen on IP and Port: 127.0.0.1:8088")
 
-l = listen(ip"127.0.0.1,8080)
-println([-] Listen on IP and Port 127.0.0.1:8080)
+cn = -1
 
 while true
-    c = aceept(l)
-    atendimento() = connect1(c)
-    schedule(Task(atendimento))
-and
+	global cn += 1
+	c = accept(l)
+	atendimento() = conecta(c, cn)
+	schedule(Task(atendimento))
+end
